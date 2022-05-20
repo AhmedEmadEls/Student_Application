b41373c3774a6e7034257b9b21a2e53ccd15862c
AhmedEmadEls <ahmedelshabrawi235@gmail.com>
2022-05-20 02:30:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/student_application/Test_Activity.java
@@ -0,0 +1,14 @@
+package com.example.student_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Test_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
