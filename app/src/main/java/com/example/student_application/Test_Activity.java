c7daa3698f8827b31ecf6c69ef81b557ba7ad112
AhmedEmadEls <ahmedelshabrawi235@gmail.com>
2022-05-20 02:43:54 +0200

    Initial commit

--- app/src/main/java/com/example/student_application/Test_Activity.java
+++ app/src/main/java/com/example/student_application/Test_Activity.java
@@ -3,6 +3,9 @@ package com.example.student_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Test_Activity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class Test_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+    }
+
+    public void m1(View view) {
+        TextView textView = findViewById(R.id.textView7);
+        ImageView imageView = findViewById(R.id.imageView2);
+
+        if (textView.getVisibility() == View.GONE){
+            textView.setVisibility(View.VISIBLE);
+            imageView.setImageResource(R.drawable.arrows_up);
+        }else {
+            textView.setVisibility(View.GONE);
+            imageView.setImageResource(R.drawable.arrows_down);
+        }
+
     }
 }
