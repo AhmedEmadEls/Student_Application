98d6fb875573112165c25f686b6aeb0f0a7af8ee
AhmedEmadEls <ahmedelshabrawi235@gmail.com>
2022-06-11 22:41:01 +0200

    add secure screen

--- app/src/main/java/com/example/student_application/Servise_Activity.java
+++ app/src/main/java/com/example/student_application/Servise_Activity.java
@@ -3,9 +3,12 @@ package com.example.student_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+
+import java.net.URL;
 
 public class Servise_Activity extends AppCompatActivity {
 
@@ -21,8 +24,16 @@ public class Servise_Activity extends AppCompatActivity {
     }
 
     public void notnaw(View view) {
-        Intent intent = new Intent(this ,NotActivity.class);
-        startActivity(intent);
+        String url = "https://forms.gle/4TTP93wTmr63GRCf6";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
 
+    public void acadimic(View view) {
+        String url = "http://comm.nilehi.edu.eg/static/index.html";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 }
